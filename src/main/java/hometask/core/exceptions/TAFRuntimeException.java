dca9bf0554f8a06e7213f267b900d3b300f3aa84
Dmytro_Prykhodko <awesome5040@gmail.com>
2021-05-09 00:09:50 +0300

    add integration with reportPortal

--- src/main/java/hometask/core/exceptions/TAFRuntimeException.java
+++ src/main/java/hometask/core/exceptions/TAFRuntimeException.java
@@ -2,8 +2,12 @@ package hometask.core.exceptions;
 
 public class TAFRuntimeException extends RuntimeException{
 
-    public TAFRuntimeException(String message, Throwable exception) {
+    public TAFRuntimeException(final String message, final Throwable exception) {
         super(message, exception);
+    }
+
+    public TAFRuntimeException(final String message) {
+        super(message);
     }
 
 }
